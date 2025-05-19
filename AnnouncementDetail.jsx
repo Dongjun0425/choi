1276125e747c860781edcb0ce9b903f209f5b4b7
HS0100 <ggs339@naver.com>
2025-05-19 15:27:09 +0900

    상세보기 페이지 수정

--- AnnouncementDetail.jsx
+++ AnnouncementDetail.jsx
@@ -11,28 +11,6 @@ function AnnouncementDetail() {
     // URL 파라미터에서 ID 값을 가져오기
     const { id } = useParams();
 
-    // 공지 목록 데이터
-    const announcements = [
-        {
-            id: 1,
-            title: "라면",
-            content: "라면을 드세요 라면을 먹어야합니다",
-            image: "https://i.namu.wiki/i/ffZ5bAtsg1fWZnRFLEpx9pUT-vKIfoxU_aBslOk1jZzJd0DQHrn283Tg_jg0F20uwwWF9E-OxLh1ur3qMq47kL7xHp0NCRp7KelX5kGduCalharqpYPfDNjd4FFC-dSCf_KWB_vx5DE2oI7_4txL2A.webp"
-        },
-        {
-            id: 2,
-            title: "참치앤김치",
-            content: "참치에는 와인!",
-            image: "https://flexible.img.hani.co.kr/flexible/normal/900/675/imgdb/original/2021/0825/20210825503877.jpg"
-        },
-        {
-            id: 3,
-            title: "오늘의 교보문고",
-            content: "BEST 추천 책",
-            image: "https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9791191114768.jpg"
-        },
-    ];
-
     // 현재 위치 정보에서 공지 데이터 가져오기, 없으면 리스트에서 ID로 검색
     const announcement = location.state || announcements.find(item => item.id === parseInt(id));
 
